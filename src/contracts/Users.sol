811f330e47065835f3905891473e56fb35f54e4e
mnava1418 <mnavapena@gmail.com>
2022-02-07 15:03:20 -0600

    ADD: follow user method and tests

--- src/contracts/Users.sol
+++ src/contracts/Users.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 contract Users {  
   address public ownerAccount;
   mapping(address => _User) public users;
+  mapping(address => mapping(address => bool)) public followings;
+  mapping(address => mapping(address => bool)) public followers;
   
   /*---OBJECTS---*/  
   struct _User {
@@ -11,7 +13,8 @@ contract Users {
     string email;
     string profilePic;
     uint cost;
-    address userAddress;    
+    address userAddress;
+    bool valid;    
   }
 
   /*---EVENTS---*/
@@ -21,6 +24,11 @@ contract Users {
     string profilePic,
     uint cost,
     address userAddress
+  );
+
+  event UserInteraction(
+    address currentUser,
+    address followedUser
   );
   
   constructor(address _ownerAccount) {
@@ -34,9 +42,26 @@ contract Users {
     require(bytes(_profilePic).length > 0, 'La foto de perfil es obligatoria');
 
     //Add user and pay the owner
-    users[msg.sender] = _User(_name, _email, _profilePic, _cost, msg.sender);
+    users[msg.sender] = _User(_name, _email, _profilePic, _cost, msg.sender, true);
     payable(ownerAccount).transfer(msg.value);
 
     emit CreateUser(_name, _email, _profilePic, _cost, msg.sender);
+  }
+
+  function followUser(address _addressToFollow) public payable {
+    require(_addressToFollow != address(0), 'Invalid user');
+    require(users[_addressToFollow].valid, 'Usuario no existe');
+
+    _User memory userToFollow = users[_addressToFollow];
+
+    if(userToFollow.cost > 0) {
+      require(userToFollow.cost == msg.value, 'Monto incorrecto');
+      payable(_addressToFollow).transfer(msg.value);
+    }
+
+    followings[msg.sender][_addressToFollow] = true;
+    followers[_addressToFollow][msg.sender] = true;
+
+    emit UserInteraction(msg.sender, _addressToFollow);    
   }
 }
--- test/Users.test.js
+++ test/Users.test.js
@@ -2,12 +2,13 @@ const convertToETH = require('../src/utils/ethUtils').convertToETH
 const web3 = require('web3')
 const eth = new web3(web3.givenProvider).eth;
 const Users = artifacts.require('./Users')
+const ETH = '0x0000000000000000000000000000000000000000'
 
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Users', ([deployer, user1]) => {
+contract('Users', ([deployer, premiumUser, freeUser, uncreatedUser, secondPremiumUser]) => {
     let users
     beforeEach(async () => {
         users = await Users.new(deployer)
@@ -20,7 +21,7 @@ contract('Users', ([deployer, user1]) => {
         })
     })
 
-    describe('create user', () => {
+    describe('createUser', () => {
         describe('success', async () => {
             const creationCost = convertToETH(0.1)
             let result
@@ -29,12 +30,12 @@ contract('Users', ([deployer, user1]) => {
                         
             beforeEach(async () => {
                 originalBalanceOwner = await eth.getBalance(deployer)
-                originalBalanceUser = await eth.getBalance(user1)
-                result = await users.createUser('name', 'test@test.com', 'profilePic', convertToETH(1), {from: user1, value: creationCost})                
+                originalBalanceUser = await eth.getBalance(premiumUser)
+                result = await users.createUser('name', 'test@test.com', 'profilePic', convertToETH(1), {from: premiumUser, value: creationCost})                
             })
 
             it('user was created', async () => {
-                const createdUser = await users.users(user1)
+                const createdUser = await users.users(premiumUser)
                 createdUser.name.should.equal('name')
                 createdUser.email.should.equal('test@test.com')
                 createdUser.profilePic.should.equal('profilePic')
@@ -61,15 +62,124 @@ contract('Users', ([deployer, user1]) => {
 
         describe('failure', () => {
             it('invalid name', async () => {
-                await users.createUser('', 'test@test.com', 'profilePic', convertToETH(1), {from: user1, value: convertToETH(0.1)}).should.be.rejected
+                await users.createUser('', 'test@test.com', 'profilePic', convertToETH(1), {from: premiumUser, value: convertToETH(0.1)}).should.be.rejected
             })
 
             it('invalid email', async () => {
-                await users.createUser('name', '', 'profilePic', convertToETH(1), {from: user1, value: convertToETH(0.1)}).should.be.rejected
+                await users.createUser('name', '', 'profilePic', convertToETH(1), {from: premiumUser, value: convertToETH(0.1)}).should.be.rejected
             })
 
             it('invalid profilePic', async () => {
-                await users.createUser('name', 'test@test.com', '', convertToETH(1), {from: user1, value: convertToETH(0.1)}).should.be.rejected
+                await users.createUser('name', 'test@test.com', '', convertToETH(1), {from: premiumUser, value: convertToETH(0.1)}).should.be.rejected
+            })
+        })
+    })
+    describe('followUser', () => {
+        describe('success', () => {
+            describe('premium user', () => {
+                const premiumCost = convertToETH(0.5)
+                let originalPremiumBalance
+                let result
+
+                beforeEach(async () => {
+                    await users.createUser('name', 'test@test.com', 'profilePic', premiumCost, {from: premiumUser, value: convertToETH(0.1)})
+                    originalPremiumBalance = await eth.getBalance(premiumUser)
+                    result = await users.followUser(premiumUser, {from: deployer, value: premiumCost})
+                })
+
+                it('payment was made', async () => {
+                    const finalPremiumBalance = await eth.getBalance(premiumUser)
+                    const expectedPremiumBalance = parseFloat(originalPremiumBalance) + parseFloat(premiumCost)
+                    expectedPremiumBalance.toString().should.equal(finalPremiumBalance.toString())
+                })
+
+                it('deployer follows premiumUser', async() => {
+                    const isFollowing = await users.followings(deployer, premiumUser)
+                    isFollowing.should.equal(true)
+                })
+
+                it('premiumUser is followed by deployer', async () => {
+                    const isFollowed = await users.followers(premiumUser, deployer)
+                    isFollowed.should.equal(true)
+                })
+
+                it('emit UserInteraction event', () => {
+                    const log = result.logs[0]
+                    const event = log.args
+        
+                    log.event.should.equal('UserInteraction')
+                    event.currentUser.should.equal(deployer)
+                    event.followedUser.should.equal(premiumUser)
+                })
+            })
+            describe('free user', () => {
+                let originalFreeBalance
+                let result
+
+                beforeEach(async () => {
+                    await users.createUser('name', 'test@test.com', 'profilePic', convertToETH(0), {from: freeUser, value: convertToETH(0.1)})
+                    originalFreeBalance = await eth.getBalance(freeUser)
+                    result = await users.followUser(freeUser, {from: deployer})
+                })
+
+                it('no payment was made', async () => {
+                    const finalFreeBalance = await eth.getBalance(freeUser)                    
+                    originalFreeBalance.toString().should.equal(finalFreeBalance.toString())
+                })
+
+                it('deployer follows freeUser', async() => {
+                    const isFollowing = await users.followings(deployer, freeUser)
+                    isFollowing.should.equal(true)
+                })
+
+                it('freeUser is followed by deployer', async () => {
+                    const isFollowed = await users.followers(freeUser, deployer)
+                    isFollowed.should.equal(true)
+                })
+
+                it('emit UserInteraction event', () => {
+                    const log = result.logs[0]
+                    const event = log.args
+        
+                    log.event.should.equal('UserInteraction')
+                    event.currentUser.should.equal(deployer)
+                    event.followedUser.should.equal(freeUser)
+                })
+            })
+        })
+        describe('failure', () => {
+            it('invalid address', async () => {
+                await users.followUser(ETH, {from: deployer}).should.be.rejected
+                const isFollowed = await users.followers(ETH, deployer)
+                const isFollowing = await users.followings(deployer, ETH)
+                isFollowed.should.equal(false)
+                isFollowing.should.equal(false)                
+            })
+
+            it('user not created', async () => {
+                await users.followUser(uncreatedUser, {from: deployer}).should.be.rejected
+                const isFollowed = await users.followers(uncreatedUser, deployer)
+                const isFollowing = await users.followings(deployer, uncreatedUser)
+                isFollowed.should.equal(false)
+                isFollowing.should.equal(false)                
+            })
+
+            it('payment not done', async () => {
+                await users.createUser('name', 'test@test.com', 'profilePic', convertToETH(0.1), {from: secondPremiumUser, value: convertToETH(0.1)})
+                await users.followUser(secondPremiumUser, {from: deployer}).should.be.rejected
+                const isFollowed = await users.followers(secondPremiumUser, deployer)
+                const isFollowing = await users.followings(deployer, secondPremiumUser)
+                isFollowed.should.equal(false)
+                isFollowing.should.equal(false)        
+            })
+
+            it('incomplete payment', async () => {
+                await users.createUser('name', 'test@test.com', 'profilePic', convertToETH(0.1), {from: secondPremiumUser, value: convertToETH(0.1)})
+                await users.followUser(secondPremiumUser, {from: deployer, value: convertToETH(0.01)}).should.be.rejected
+                const isFollowed = await users.followers(secondPremiumUser, deployer)
+                const isFollowing = await users.followings(deployer, secondPremiumUser)
+                isFollowed.should.equal(false)
+                isFollowing.should.equal(false)  
             })
         })
     })
