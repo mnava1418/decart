2c80a8c8a5ad9cbebc72306afe5cc7a8bd69a651
mnava1418 <mnavapena@gmail.com>
2022-02-09 19:24:23 -0600

    ADD: nav bar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,13 +7,20 @@ import '../styles/NavBar.css'
 function NavBar() {
     return (
         <div className='myNavBar-main'>
-          <div className='myNavBar-logo'>
+          <div className='myNavBar-element myNavBar-logo'>
               <img src={logo} alt='Decart' width={56} height={56}/>
               <h3>Decart</h3>
-          </div>          
-          <div>
-            <Button variant='primary'>Conectar</Button>
-          </div>
+          </div>  
+          <div className='myNavBar-element'>
+            <div className='myNavBar-links'>
+              <i class="bi bi-house-fill"></i>
+              <i class="bi bi-person-fill"></i>
+              <i class="bi bi-plus-circle-fill"></i>
+            </div>
+            <div className='myNavBar-links'>
+              <Button variant='primary'>Conectar</Button>
+            </div>
+          </div>        
         </div>
     );
 }
