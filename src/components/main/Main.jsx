b0d6f072cdcc17c479fa19ae087ed3f79ddb970c
mnava1418 <mnavapena@gmail.com>
2022-02-17 20:05:44 -0600

    ADD: first version for main layout and styles

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,8 +1,100 @@
 import React from 'react'
+import { Card, Button } from 'react-bootstrap'
+
+import '../../styles/Main.css'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div className='main-container'>
+      <div className='feed-container d-flex flex-column justify-content-start align-items-center'>
+        <Card className='main-element feed-element'>
+          <Card.Body>
+            <Card.Title>Foto 1</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className='main-element feed-element'>
+          <Card.Body>
+            <Card.Title>Foto 2</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className='main-element feed-element'>
+          <Card.Body>
+          <Card.Title>Foto 3</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className='main-element feed-element'>
+          <Card.Body>
+            <Card.Title>Foto 4</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className='main-element feed-element'>
+          <Card.Body>
+            <Card.Title>Foto 5</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className='main-secondary-section'>
+        <Card className='main-element' style={{ width: '18rem' }}>          
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card className='main-element' style={{ width: '18rem' }}>          
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card className='main-element' style={{ width: '18rem' }}>          
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
   )
 }
 
