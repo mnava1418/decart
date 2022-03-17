225ea1a21cb3caa92a27c9563dbd191ea5b9948a
mnava1418 <mnavapena@gmail.com>
2022-03-16 19:56:04 -0600

    UPDATE: show user data in userFeed component

---
+++ src/components/main/UserData.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import { Button, OverlayTrigger, Tooltip } from 'react-bootstrap'
+import { ipfsData } from '../../config'
+import { getUserNumbers } from '../helpers'
+import { fromWei } from '../../services/ethService'
+
+function UserData({selectedUser, currentUser}) {  
+  const [tooltipText, setToolTipText] = useState('Copiar al Portapapeles')
+
+  const formatAddress = (address) => {
+    return `${address.substring(0,5)}...${address.substring(address.length - 4, address.length)}`
+  }  
+
+  const copyAddress = (address) => {
+    const elem = document.createElement('textarea');
+    elem.value = address;
+    document.body.appendChild(elem);
+    elem.select();
+    document.execCommand('copy');
+    document.body.removeChild(elem);
+    setToolTipText('Copiado!')
+  }
+
+  const getFollowBtn = () => {
+      return(
+        <Button variant="primary">Follow User</Button>
+      )
+    }
+
+  const getUserData = () => {
+    return(
+      <div className='d-flex flex-column justify-content-center align-items-start user-feed-data'>
+        <OverlayTrigger placement='bottom' overlay={<Tooltip id="tooltip-copy">{tooltipText}</Tooltip>} onExited={() => {setToolTipText('Copiar al Portapapeles')}}>          
+          <div style={{cursor: 'pointer'}} onClick={() => {copyAddress(selectedUser.userAddress)}}><i class="bi bi-wallet"></i> {formatAddress(selectedUser.userAddress)}</div>
+        </OverlayTrigger>
+        <div><i class="bi bi-envelope"></i> {selectedUser.email}</div>
+        <div>Membresía: {fromWei(parseFloat(selectedUser.cost))} ETH</div>
+      </div>
+    )
+  }
+
+  return (
+    <>
+        <div className='d-flex flex-row justify-content-center align-items-start' style={{width: '95%'}}>
+          <div className='bg-image bg-image-cover user-feed-pic'>
+            <img alt={selectedUser.name} src={`${ipfsData.protocol}://${ipfsData.host}/ipfs/${selectedUser.profilePic}`}/>
+          </div>
+          <div className='user-feed-info' >
+            <div className='d-flex user-feed-primary'>
+              <span className='user-feed-title'>{selectedUser.name}</span>
+              {currentUser.userAddress === selectedUser.userAddress ? <></> : getFollowBtn()}
+            </div>
+            <div className='user-feed-not-mobile'>
+              {getUserNumbers(parseFloat(selectedUser.posts), parseFloat(selectedUser.followers), parseFloat(selectedUser.followings))}
+            </div>
+            <div className='user-feed-not-mobile'>
+              {getUserData()}              
+            </div>
+          </div>
+        </div>
+        <div className='user-feed-mobile'>
+          {getUserData()}              
+        </div>
+        <div className='user-feed-mobile'>
+              {getUserNumbers(parseFloat(selectedUser.posts), parseFloat(selectedUser.followers), parseFloat(selectedUser.followings))}
+        </div>  
+    </>
+  )
+}
+
+export default UserData
--- src/components/main/UserFeed.jsx
+++ src/components/main/UserFeed.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import { currentUserSelector, selectedUserSelector } from '../../store/slices/usersSlice'
+import { Spinner } from 'react-bootstrap'
+import UserData from './UserData'
+
+import '../../styles/UserFeed.css'
 
 function UserFeed() {
+  const selectedUser = useSelector(selectedUserSelector)  
+  const currentUser = useSelector(currentUserSelector)
+  
+  const generateUserFeed = () => {
+    return(
+      <div className='d-flex flex-column justify-content-start align-items-center user-feed-container'>
+        <UserData selectedUser={selectedUser} currentUser={currentUser}/>
+      </div>
+    )
+  }
+
   return (
-    <div>User Feed</div>
+    <>
+      {!selectedUser ? <Spinner animation="grow" /> : generateUserFeed()}
+    </>    
   )
 }
 
