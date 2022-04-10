ec75c8f60129097508be3417a39803fad10a39d9
mnava1418 <mnavapena@gmail.com>
2022-04-09 22:38:23 -0500

    UPDATE: add post method and improve error handling

--- src/services/networkService.js
+++ src/services/networkService.js
@@ -1,16 +1,37 @@
 import axios from 'axios'
 
-const getInstance = (baseURL, headers) => {
-    const instance = axios.create({ baseURL, headers})
+const getInstance = (baseURL, headers, token) => {
+    const instance = axios.create({
+        baseURL, 
+        headers: {...headers, 'Authorization': token ? token : ''}
+    })
     return instance    
 }
 
-export const get = async (baseURL, path, headers) => {
-    const instance = getInstance(baseURL, headers)
+export const get = async (baseURL, path, token = undefined, headers = {'Content-Type': 'application/json'}) => {
+    const instance = getInstance(baseURL, headers, token)
     const result = await instance.get(path).catch(error => {
-        console.error(error)
-        return {status: 500, data:{error, errorMessage: 'Unexpected error.'}}
+        return processError(error)
     })
 
     return result
+}
+
+export const post = async (baseURL, path, data, token = undefined, headers = {'Content-Type': 'application/json'}) => {
+    const instance = getInstance(baseURL, headers, token)
+    const result = await instance.post(path, data ).catch(error => {
+        return processError(error)
+    })
+
+    return result
+}
+
+const processError = (error) => {
+    console.error(error.config);
+    
+    if (error.response) {
+        return {status: error.response.status, data: error.response.data}
+    } else {
+        return {status: 500, data:{error, message: 'Unexpected error.'}}
+    }
 }
