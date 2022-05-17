70e2fdd16d8d5204e8b1de26f8d64a2c1f8bac87
mnava1418 <martin@mnavapena.com>
2022-05-16 19:59:06 -0500

    UPDATE: add icon to not found page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -6,6 +6,7 @@ function NotFound() {
   return (
     <div className='d-flex flex-column justify-content-center align-items-center bg-image bg-image-cover not-found-container'>
         <div className='d-flex flex-column justify-content-center align-items-center' style={{width: '80%'}}>
+            <h2><i class="bi bi-diagram-3"></i></h2>
             <h1>404</h1>
             <h5>Oops! The page you requested was not found!</h5>
             <a className='not-found-link' href='/'>Back to Home</a>
