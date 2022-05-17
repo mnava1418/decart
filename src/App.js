a95c7aace470162ecd7494af911352f5b3330f4b
mnava1418 <martin@mnavapena.com>
2022-05-16 19:52:31 -0500

    ADD: create not found page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Root from './Root';
 import Terms from './components/Terms';
+import NotFound from './components/NotFound';
 import { PATHS } from './config';
 
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path={PATHS.index} element={<Root />} />
         <Route path={PATHS.terms} element={<Terms />} />
+        <Route path= "*" element={<NotFound />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import '../styles/NotFound.css'
+
+function NotFound() {
+  return (
+    <div className='d-flex flex-column justify-content-center align-items-center bg-image bg-image-cover not-found-container'>
+        <div className='d-flex flex-column justify-content-center align-items-center' style={{width: '80%'}}>
+            <h1>404</h1>
+            <h5>Oops! The page you requested was not found!</h5>
+            <a className='not-found-link' href='/'>Back to Home</a>
+        </div>
+    </div>
+  )
+}
+
+export default NotFound
